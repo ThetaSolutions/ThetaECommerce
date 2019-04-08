451f103005ec8b750a79ce14bbd2caf962b10aeb
g4genious <g_4_genious@yahoo.com>
2019-04-08 14:40:35 +0500

    Add project files.

---
+++ ThetaECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThetaECommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
